da8ae7eba88e613c2d25af8ef140fda35d78f9f1
ramikong <57300746+ramikong@users.noreply.github.com>
2020-01-30 01:53:07 +0200

    Add files via upload

---
+++ Fastrun.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+cd $HOME/Rambocli
+rm -rf $HOME/.telegram-cli
+Rambocliins() {
+sudo chmod +x tg
+chmod +x Rambocli
+chmod +x oy
+./ts
+}
+if [ "$1" = "ins" ]; then
+Rambocliins
+fi
+chmod +x Run.sh
+lua Run.lua
